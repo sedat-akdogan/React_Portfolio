ca2b2e43c19f9bc96ff0cc16fe76f3fbfe876171
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-05-21 16:41:10 +0100

    navbar section is completed

--- .history/src/App_20230521163505.jsx
+++ .history/src/App_20230521163505.jsx
@@ -6,9 +6,8 @@ const App = () => {
     return (
         <BrowserRouter>
             <div className='relative z-0 bg-primary'>
-                <Navbar />
                 <div className='bg-hero-pattern bg-cover bg- 
-                    no-repeat bg-center'>
+                no-repeat bg-center'>
                     <Hero />
                 </div>
                 <About />
--- .history/src/components/Navbar_20230521155355.jsx
+++ .history/src/components/Navbar_20230521155355.jsx
@@ -8,11 +8,27 @@ import { sedat, menu, close } from "../assets";
 const Navbar = () => {
     const [active, setActive] = useState("");
     const [toggle, setToggle] = useState(false);
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollTop = window.scrollY;
+            if (scrollTop > 100) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
 
     return (
         <nav
             className={`${styles.paddingX} w-full flex items-center py-5 fixed top- 
-            0 z-20 bg-primary}`}
+            0 z-20 ${scrolled ? "bg-primary" : "bg-transparent"}`}
         >
             <div className='w-full flex justify-between items-center max-w-7xl mx- 
             auto'

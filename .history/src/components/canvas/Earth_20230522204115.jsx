c6a5473d362e2051091af2a8854544bb8462b828
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-05-22 21:29:45 +0100

    Earth component is completed

--- .history/src/components/canvas/Earth_20230522204115.jsx
+++ .history/src/components/canvas/Earth_20230522204115.jsx
@@ -1,4 +1,4 @@
-import { Suspense } from "react";
+import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 

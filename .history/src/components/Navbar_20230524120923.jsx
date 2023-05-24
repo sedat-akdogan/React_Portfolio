00416da56ed80fe33bd49e3e589b6fe8c49c885d
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-05-24 12:15:28 +0100

    navbar is updated

--- .history/src/components/Navbar_20230524120923.jsx
+++ .history/src/components/Navbar_20230524120923.jsx
@@ -27,11 +27,12 @@ const Navbar = () => {
 
     return (
         <nav
-            className={`${styles.paddingX
-                } w-full flex items-center py-5 fixed top-0 z-20 ${scrolled ? "bg-primary" : "bg-transparent"
-                }`}
+            className={`${styles.paddingX} w-full h-20 flex items-center py-5 fixed top- 
+            0 z-50 bg-primary}`}
         >
-            <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+            <div className='w-full flex justify-between items-center max-w-7xl mx- 
+            auto z-50 bg-primary absolute inset-y-0'
+            >
                 <Link
                     to='/'
                     className='flex items-center gap-2'
--- .history/src/components/Navbar_20230524121109.jsx
+++ .history/src/components/Navbar_20230524121109.jsx
@@ -27,11 +27,13 @@ const Navbar = () => {
 
     return (
         <nav
-            className={`${styles.paddingX
-                } w-full flex items-center py-5 fixed top-0 z-20 ${scrolled ? "bg-primary" : "bg-transparent"
-                }`}
-        >
-            <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+        className={`${
+          styles.paddingX
+        } w-full flex items-center py-5 fixed top-0 z-20 ${
+          scrolled ? "bg-primary" : "bg-transparent"
+        }`}
+      >
+        <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
                 <Link
                     to='/'
                     className='flex items-center gap-2'
